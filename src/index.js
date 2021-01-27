6d00b2c67e1403b1eb5d45e4f4a4eb4385b09650
Alex <salexandervl@gmail.com>
2021-01-28 01:53:08 +0300

    'lesson2-begin'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const el = React.createElement(
+//   'h1',
+//   null,
+//   'Hello World, React.js!'
+// );
+
+const el = <h1>Hello World!</h1>
+
+ReactDOM.render(el, document.getElementById('root'));
